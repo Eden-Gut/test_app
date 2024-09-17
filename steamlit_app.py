768b93b823ba3053b741160ad4eafdea92588452
Eden-Gut <Edetgut@gmail.com>
2024-09-17 15:27:53 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -3,43 +3,6 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-#######################
-# CSS styling
-st.markdown("""
-<style>
-
-[data-testid="stMetric"] {
-    background-color: #393939;
-    text-align: center;
-    border-color: #FFFF00
-    padding: 15px 0;
-}
-
-[data-testid="stMetricLabel"] {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-[data-testid="stMetricDeltaIcon-Up"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-[data-testid="stMetricDeltaIcon-Down"] {
-    position: relative;
-    left: 38%;
-    -webkit-transform: translateX(-50%);
-    -ms-transform: translateX(-50%);
-    transform: translateX(-50%);
-}
-
-</style>
-""", unsafe_allow_html=True)
-
 # פונקציה לשינוי פורמט עמודות
 def change_column_format(df, column):
     st.write("### Change Column Format")
