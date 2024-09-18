74e5cc87c520afbc98463edeb78eaea630e750b6
Eden-Gut <Edetgut@gmail.com>
2024-09-18 09:16:46 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -92,7 +92,8 @@ def change_column_format(df, column):
     if st.button("Save Format"):
         st.session_state["column_formats"][column] = format_choice
         st.success(f"Format for column '{column}' saved as {format_choice}.")
-
+        
+    columns = st.multiselect("Columns:",df.columns)
     st.dataframe(df, use_container_width=True)
 
 # פונקציה להצגת סטטיסטיקות עבור עמודות מספריות
