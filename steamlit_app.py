7c5b09f1a7b7c38e6db97d7d5dd830f4abfa08c9
Eden-Gut <Edetgut@gmail.com>
2024-09-23 13:39:13 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -227,7 +227,7 @@ def show_missing_data(df):
 uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
 if uploaded_file:
-     st.sidebar.title("navigation")
+    st.sidebar.title("navigation")
     df = pd.read_csv(uploaded_file)
     
     with st.expander("Data Preview", expanded=True):
